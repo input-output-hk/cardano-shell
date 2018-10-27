8fc1f3f0def831dcf0cb06939761f751aa57d244
Rob Cohen <rob.cohen@iohk.io>
2018-10-27 09:27:58 -0400

    #3 - Attempt to simplify nixpkgs pinning

--- release.nix
+++ release.nix
@@ -9,10 +9,12 @@ let
       };
     };
   };
+  nixpkgs = builtins.fetchTarball {
+    url    = "https://github.com/NixOS/nixpkgs/archive/069bf7aee30faf7b3ed773cfae2154d761b2d6c2.tar.gz";
+    sha256 = "1c44vjb60fw2r8ck8yqwkj1w4288wixi59c6w1vazjixa79mvjvg";
+  };
 
-  # pinning
-  fetchNixPkgs  = import ./scripts/nix/fetch-nixpkgs.nix;
-  pkgs          = import fetchNixPkgs { inherit config; };
+  pkgs = import nixpkgs { inherit config; };
  in
   { cardano-shell = pkgs.haskellPackages.cardano-shell;
   }
