03d641f2ae3a4b1689cfacd3f73e590b0c7fc7c8
Rob Cohen <rob.cohen@iohk.io>
2018-10-29 09:30:59 -0400

    Used let pattern

--- stack-shell.nix
+++ stack-shell.nix
@@ -1,6 +1,9 @@
 { system ? builtins.currentSystem
 , config ? {}
 }:
+
+let
+
   nixpkgs = builtins.fetchTarball {
     url    = "https://github.com/NixOS/nixpkgs/archive/069bf7aee30faf7b3ed773cfae2154d761b2d6c2.tar.gz";
     sha256 = "1c44vjb60fw2r8ck8yqwkj1w4288wixi59c6w1vazjixa79mvjvg";
@@ -8,7 +11,7 @@
 
   pkgs = import nixpkgs { inherit config; };
 
-with pkgs;
+in 
 
 haskell.lib.buildStackProject {
   name = "cardano-shell-env";
