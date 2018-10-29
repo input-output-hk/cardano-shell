1997a145fbc0f7d2a653ac8f6b3f8db607cef6a5
Rob Cohen <rob.cohen@iohk.io>
2018-10-29 09:12:18 -0400

    Fix stack

--- stack-shell.nix
+++ stack-shell.nix
@@ -1,7 +1,12 @@
 { system ? builtins.currentSystem
 , config ? {}
-, pkgs ? import (import ./scripts/nix/fetch-nixpkgs.nix) { inherit system config; }
 }:
+  nixpkgs = builtins.fetchTarball {
+    url    = "https://github.com/NixOS/nixpkgs/archive/069bf7aee30faf7b3ed773cfae2154d761b2d6c2.tar.gz";
+    sha256 = "1c44vjb60fw2r8ck8yqwkj1w4288wixi59c6w1vazjixa79mvjvg";
+  };
+
+  pkgs = import nixpkgs { inherit config; };
 
 with pkgs;
 
