febb8c57de82f756ddf46940619f942a2144d2ed
Hiroto Shioi <hiroto.shioi@iohk.io>
2019-02-20 14:14:08 +0900

    Remove netcat

--- stack-shell.nix
+++ stack-shell.nix
@@ -16,6 +16,6 @@ in
 
   haskell.lib.buildStackProject {
     name = "cardano-shell-env";
-    buildInputs = [ zlib openssl git netcat ];
+    buildInputs = [ zlib openssl git ];
     ghc = haskell.packages.ghc863.ghc;
   }
