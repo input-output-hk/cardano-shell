a91cd08e8a991cee8c7235bfa425767fd03bb9eb
Alexander Diemand <codieplusplus@apax.net>
2019-04-01 18:27:43 +0200

    updating logging to release 0.1.5.0
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- iohk-monitoring-contra-tracer.nix
+++ iohk-monitoring-contra-tracer.nix
@@ -2,15 +2,15 @@
 , stdenv, tasty, tasty-hunit, tasty-quickcheck, text, unix
 }:
 mkDerivation {
-  pname = "basic-tracer";
+  pname = "contra-tracer";
   version = "0.1.0.0";
   src = fetchgit {
     url = "https://github.com/input-output-hk/iohk-monitoring-framework";
-    sha256 = "1kvsx5bz9hnkwipz9zphcnangqbmqzj266xkb0kxc61sdf2yqhv5";
-    rev = "cdbcf35ad7a3b8f90c95f0dc42f7fb111d2d689c";
+    sha256 = "16sxwx8y2wg8kws15ybhk9vkq6crs5bp7ky37x1vrvpvb3ilc5x0";
+    rev = "8fb87e83468831289820ef9edb3d5ef912b0db0f";
     fetchSubmodules = true;
   };
-  postUnpack = "sourceRoot+=/basic-tracer; echo source root reset to $sourceRoot";
+  postUnpack = "sourceRoot+=/contra-tracer; echo source root reset to $sourceRoot";
   libraryHaskellDepends = [ base contravariant text unix ];
   testHaskellDepends = [
     base QuickCheck random tasty tasty-hunit tasty-quickcheck text
