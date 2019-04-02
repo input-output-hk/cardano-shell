46d50081ca396ca5a1ebca145ce5115cfc8757e2
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-04-03 09:36:19 +1000

    fixup! [DEVOPS-1120] nix-tools autogenerated code + manual fixes

---
+++ nix/cardano-prelude-test.nix
@@ -0,0 +1,47 @@
+{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = { development = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "cardano-prelude-test"; version = "0.1.0.0"; };
+      license = "MIT";
+      copyright = "2018 IOHK";
+      maintainer = "operations@iohk.io";
+      author = "IOHK";
+      homepage = "";
+      url = "";
+      synopsis = "Utility types and functions for testing Cardano";
+      description = "Utility types and functions for testing Cardano";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs.base)
+          (hsPkgs.aeson)
+          (hsPkgs.aeson-pretty)
+          (hsPkgs.attoparsec)
+          (hsPkgs.base16-bytestring)
+          (hsPkgs.bytestring)
+          (hsPkgs.canonical-json)
+          (hsPkgs.cardano-prelude)
+          (hsPkgs.cryptonite)
+          (hsPkgs.formatting)
+          (hsPkgs.hedgehog)
+          (hsPkgs.hspec)
+          (hsPkgs.pretty-show)
+          (hsPkgs.QuickCheck)
+          (hsPkgs.quickcheck-instances)
+          (hsPkgs.text)
+          (hsPkgs.time)
+          ];
+        };
+      };
+    } // {
+    src = (pkgs.lib).mkDefault (pkgs.fetchgit {
+      url = "https://github.com/input-output-hk/cardano-prelude";
+      rev = "2256fd727c5f92e6218afdcf8cddf6e01c4a9dcd";
+      sha256 = "0fdwlhkpc7inkqflcdzinx9qr5g3i34clzhl6iiagj851c3jcgsn";
+      });
+    postUnpack = "sourceRoot+=/test; echo source root reset to \$sourceRoot";
+    }
---
+++ nix/cardano-prelude.nix
@@ -0,0 +1,48 @@
+{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = { development = false; };
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "cardano-prelude"; version = "0.1.0.0"; };
+      license = "MIT";
+      copyright = "2018 IOHK";
+      maintainer = "operations@iohk.io";
+      author = "IOHK";
+      homepage = "";
+      url = "";
+      synopsis = "A Prelude replacement for the Cardano project";
+      description = "A Prelude replacement for the Cardano project";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs.base)
+          (hsPkgs.aeson)
+          (hsPkgs.array)
+          (hsPkgs.base16-bytestring)
+          (hsPkgs.bytestring)
+          (hsPkgs.canonical-json)
+          (hsPkgs.cborg)
+          (hsPkgs.containers)
+          (hsPkgs.formatting)
+          (hsPkgs.ghc-prim)
+          (hsPkgs.hashable)
+          (hsPkgs.integer-gmp)
+          (hsPkgs.mtl)
+          (hsPkgs.nonempty-containers)
+          (hsPkgs.protolude)
+          (hsPkgs.tagged)
+          (hsPkgs.text)
+          (hsPkgs.time)
+          (hsPkgs.vector)
+          ];
+        };
+      };
+    } // {
+    src = (pkgs.lib).mkDefault (pkgs.fetchgit {
+      url = "https://github.com/input-output-hk/cardano-prelude";
+      rev = "2256fd727c5f92e6218afdcf8cddf6e01c4a9dcd";
+      sha256 = "0fdwlhkpc7inkqflcdzinx9qr5g3i34clzhl6iiagj851c3jcgsn";
+      });
+    }
