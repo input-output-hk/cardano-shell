e8d1614e56a799da69b145629bd1cc7e0bae5066
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2019-04-03 17:02:02 +0200

    Update build to use nix-tools.

--- nix/.stack.nix/cardano-prelude-test.nix
+++ nix/.stack.nix/cardano-prelude-test.nix
@@ -25,7 +25,10 @@
           (hsPkgs.bytestring)
           (hsPkgs.canonical-json)
           (hsPkgs.cardano-prelude)
+<<<<<<< HEAD
           (hsPkgs.containers)
+=======
+>>>>>>> Update build to use nix-tools.
           (hsPkgs.cryptonite)
           (hsPkgs.formatting)
           (hsPkgs.hedgehog)
@@ -34,7 +37,10 @@
           (hsPkgs.QuickCheck)
           (hsPkgs.quickcheck-instances)
           (hsPkgs.text)
+<<<<<<< HEAD
           (hsPkgs.template-haskell)
+=======
+>>>>>>> Update build to use nix-tools.
           (hsPkgs.time)
           ];
         };
@@ -42,8 +48,13 @@
     } // {
     src = (pkgs.lib).mkDefault (pkgs.fetchgit {
       url = "https://github.com/input-output-hk/cardano-prelude";
+<<<<<<< HEAD
       rev = "892661cc3951e0d3a385c49c4563b2ae1cf9a4c2";
       sha256 = "0nnplb68q18bcbgl49dh0m3lppm7v5ys1svk9q3kpl0yihydh9s5";
+=======
+      rev = "2256fd727c5f92e6218afdcf8cddf6e01c4a9dcd";
+      sha256 = "0fdwlhkpc7inkqflcdzinx9qr5g3i34clzhl6iiagj851c3jcgsn";
+>>>>>>> Update build to use nix-tools.
       });
     postUnpack = "sourceRoot+=/test; echo source root reset to \$sourceRoot";
     }
