55e92954d8c35327540dc1ef7adffb8eb4b10106
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2019-04-03 17:02:02 +0200

    Update build to use nix-tools.

---
+++ nix/.stack.nix/cardano-shell.nix
@@ -0,0 +1,96 @@
+{ system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "cardano-shell"; version = "0.1.0.0"; };
+      license = "MIT";
+      copyright = "2018 IOHK";
+      maintainer = "operations@iohk.io";
+      author = "IOHK";
+      homepage = "https://github.com/input-output-hk/cardano-shell#readme";
+      url = "";
+      synopsis = "";
+      description = "Please see the README on GitHub at <https://github.com/githubuser/cardano-shell#readme>";
+      buildType = "Simple";
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs.aeson)
+          (hsPkgs.base)
+          (hsPkgs.binary)
+          (hsPkgs.bytestring)
+          (hsPkgs.cardano-prelude)
+          (hsPkgs.Cabal)
+          (hsPkgs.concurrency)
+          (hsPkgs.containers)
+          (hsPkgs.directory)
+          (hsPkgs.formatting)
+          (hsPkgs.iohk-monitoring)
+          (hsPkgs.safe-exceptions)
+          (hsPkgs.text)
+          (hsPkgs.transformers)
+          (hsPkgs.unix)
+          (hsPkgs.contravariant)
+          (hsPkgs.dhall)
+          (hsPkgs.ekg-core)
+          (hsPkgs.process)
+          (hsPkgs.QuickCheck)
+          (hsPkgs.text)
+          (hsPkgs.transformers)
+          (hsPkgs.unix)
+          ];
+        };
+      exes = {
+        "cardano-shell-exe" = {
+          depends = [
+            (hsPkgs.base)
+            (hsPkgs.cardano-shell)
+            (hsPkgs.cardano-prelude)
+            (hsPkgs.safe-exceptions)
+            (hsPkgs.iohk-monitoring)
+            ];
+          };
+        "node-ipc" = {
+          depends = [
+            (hsPkgs.base)
+            (hsPkgs.cardano-shell)
+            (hsPkgs.cardano-prelude)
+            ];
+          };
+        "cardano-launcher" = {
+          depends = [
+            (hsPkgs.base)
+            (hsPkgs.cardano-shell)
+            (hsPkgs.cardano-prelude)
+            (hsPkgs.cardano-sl-x509)
+            (hsPkgs.process)
+            (hsPkgs.unix)
+            (hsPkgs.directory)
+            (hsPkgs.filepath)
+            (hsPkgs.formatting)
+            (hsPkgs.safe-exceptions)
+            ];
+          };
+        };
+      tests = {
+        "cardano-shell-test" = {
+          depends = [
+            (hsPkgs.aeson)
+            (hsPkgs.base)
+            (hsPkgs.cardano-shell)
+            (hsPkgs.cardano-prelude)
+            (hsPkgs.dhall)
+            (hsPkgs.safe-exceptions)
+            (hsPkgs.QuickCheck)
+            (hsPkgs.hspec)
+            (hsPkgs.hspec-contrib)
+            (hsPkgs.concurrency)
+            (hsPkgs.dejafu)
+            (hsPkgs.hunit-dejafu)
+            ];
+          };
+        };
+      };
+    } // rec { src = (pkgs.lib).mkDefault ../.././.; }
