dbe720f5001bdf61c3af1f0bc144615f6a5b5667
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2019-04-05 12:08:55 +0200

    Fix nix-shells.

--- shell.nix
+++ shell.nix
@@ -1,24 +1,9 @@
-let
-  commonLib = import ./nix/iohk-common.nix;
-in
 { system ? builtins.currentSystem
 , config ? {}
 , localPkgs ? import ./. { inherit config system; }
-, pkgs ? commonLib.getPkgs { inherit config system; }
 }:
 let
   mainShell = localPkgs.nix-tools.libs.cardano-shell.env;
-  runCoveralls = pkgs.stdenv.mkDerivation {
-    name = "run-coveralls";
-    buildInputs = [ pkgs.haskellPackages.stack-hpc-coveralls pkgs.stack ];
-    shellHook = ''
-      echo '~~~ stack test'
-      stack test --coverage
-      echo '~~~ shc'
-      shc --repo-token=$COVERALLS_REPO_TOKEN cardano-shell cardano-shell-test
-      exit
-    '';
-  };
 in mainShell // {
-  inherit runCoveralls;
+  inherit (localPkgs) runCoveralls;
 }
--- stack-shell.nix
+++ stack-shell.nix
@@ -1,10 +1,4 @@
 { system ? builtins.currentSystem
 , config ? {}
-, localPkgs ? import ./. { inherit config system; }
-, pkgs ? localPkgs._lib.getPkgs { inherit config system; }
 }:
-with pkgs; haskell.lib.buildStackProject {
-  name = "cardano-shell-env";
-  buildInputs = [ zlib openssl git ];
-  ghc = haskell.packages.ghc863.ghc;
-}
+(import ./default.nix { inherit system config; }).stack-env
