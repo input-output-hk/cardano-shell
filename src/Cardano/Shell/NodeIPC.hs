4f1fb1a2bf366fe4cc129c5a86088e65de9e50df
Kristijan Šarić <kristijan.saric@iohk.io>
2019-05-29 10:47:39 +0200

    [GH-96] T1.2.1 QuickCheck state machine tests, added IPC. (#167)
    * [GH-96] Implement actual test from node IPC.
    
    * [GH-96] Fix Nix dependency.

--- src/Cardano/Shell/NodeIPC.hs
+++ src/Cardano/Shell/NodeIPC.hs
@@ -16,6 +16,15 @@ module Cardano.Shell.NodeIPC
     , ProtocolDuration (..)
     , startNodeJsIPC
     , startIPC
+    , handleIPCProtocol
+    , clientIPCListener
+    , testStartNodeIPC
+    , ServerHandles (..)
+    , ClientHandles (..)
+    , closeFullDuplexAnonPipesHandles
+    , createFullDuplexAnonPipesHandles
+    , bracketFullDuplexAnonPipesHandles
+    , serverReadWrite
      -- ** Exceptions
     , NodeIPCException(..)
     , MessageSendFailure(..)
@@ -41,14 +50,22 @@ import           Cardano.Shell.NodeIPC.Example (exampleWithFD,
                                                 exampleWithProcess,
                                                 getReadWriteHandles)
 #endif
-import           Cardano.Shell.NodeIPC.Lib (MessageSendFailure (..), MsgIn (..),
+import           Cardano.Shell.NodeIPC.Lib (ClientHandles (..),
+                                            MessageSendFailure (..), MsgIn (..),
                                             MsgOut (..), NodeIPCException (..),
                                             Port (..), ProtocolDuration (..),
-                                            isHandleClosed, isIPCException,
+                                            ServerHandles (..),
+                                            bracketFullDuplexAnonPipesHandles,
+                                            clientIPCListener,
+                                            closeFullDuplexAnonPipesHandles,
+                                            createFullDuplexAnonPipesHandles,
+                                            handleIPCProtocol, isHandleClosed,
+                                            isIPCException,
                                             isNodeChannelCannotBeFound,
                                             isUnreadableHandle,
-                                            isUnwritableHandle, startIPC,
-                                            startNodeJsIPC)
+                                            isUnwritableHandle, serverReadWrite,
+                                            startIPC, startNodeJsIPC,
+                                            testStartNodeIPC)
 import           Cardano.Shell.NodeIPC.Message (MessageException (..),
                                                 ReadHandle (..),
                                                 WriteHandle (..), readMessage,
