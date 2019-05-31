6802a7bfdfac9d6aed8e16f1db48e7ec9f103a17
Hiroto Shioi <hiroto.shioi@iohk.io>
2019-05-31 09:12:19 +0900

    Fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,3 @@
-{ system ? builtins.currentSystem
-, config ? {}
-, localPkgs ? import ./. { inherit config system; }
-}:
 let
   localPkgs = import ./. { };
   mainShell = localPkgs.nix-tools.libs.cardano-shell;
