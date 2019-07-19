a108ac45b9869a93fe2ba7f91291f2549c26d730
Hiroto Shioi <hiroto.shioi@iohk.io>
2019-07-19 11:49:34 +0900

    Add windowdUpdater runner

--- src/Cardano/Shell/Update/Lib.hs
+++ src/Cardano/Shell/Update/Lib.hs
@@ -1,7 +1,9 @@
 {-| Update module
 -}
 
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE CPP                 #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Cardano.Shell.Update.Lib
     ( UpdaterData(..)
@@ -11,11 +13,15 @@ module Cardano.Shell.Update.Lib
 
 import           Cardano.Prelude
 
+import qualified Data.Text as T
 import           Distribution.System (OS (..), buildOS)
-import           System.Directory (doesFileExist)
-import           System.Process (proc, withCreateProcess)
+import           System.Directory (doesFileExist, removeFile)
+import           System.Environment (getExecutablePath)
+import           System.Process (proc, withCreateProcess, waitForProcess)
+#ifdef mingw32_HOST_OS
+import           System.Win32.Process (getCurrentProcessId)
+#endif
 
-                                                            
 -- | Updater path, args, windows runner path, archive path
 data UpdaterData = UpdaterData
     { udPath        :: !FilePath
@@ -30,24 +36,73 @@ updaterData = case buildOS of
     OSX     -> UpdaterData "MacPath" [] Nothing (Just "ArchivePath")
     _       -> UpdaterData "LinuxPath" [] Nothing (Just "LinuxPath")
 
+data UpdateError
+    = UpdateFailed Int
+    | UpdaterDoesNotExist
+    deriving Show
+
 -- | Run the update system
-runUpdater :: UpdaterData -> IO ()
+--
+-- For UNIX system:
+--
+-- Check that @udPath@ exists, then run the command @udPath udArgs udArchivePath@
+--
+-- For Windows:
+--
+-- Check that @udPath@ exists, but instead of running the command directly, you
+-- first have to generate a @.bat@ file which will act as a script.
+-- After it being generated, you run that script.
+runUpdater :: UpdaterData -> IO (Either UpdateError ())
 runUpdater ud = do
     let path = udPath ud
     let args = map toS $ udArgs ud
     let mWindowPath = udWindowsPath ud
     let archive = maybe [] (\arch -> [toS arch]) (udArchivePath ud)
-    whenM (doesFileExist path) $ do
-        case mWindowPath of
-            Nothing -> runUnixUpdater path args archive
-            -- WIP
-            Just windowsPath -> runWindowUpdater windowsPath args archive
+    updaterExists <- doesFileExist path
+    if updaterExists
+        then do
+            exitCode <- case mWindowPath of
+                Nothing -> runCmd path args archive
+                Just windowsPath -> do
+                    writeWindowsUpdaterRunner windowsPath
+                    runCmd windowsPath args archive
+            case exitCode of
+                ExitSuccess -> do
+                    whenJust (udArchivePath ud) $ \updateArchivePath -> do
+                        removeFile updateArchivePath
+                    return . Right $ ()
+                ExitFailure code -> return . Left $ UpdateFailed code
+        else return . Left $ UpdaterDoesNotExist
   where
-    runUnixUpdater path args archive = 
+    runCmd path args archive =
         withCreateProcess (proc path (args <> archive))
-            $ \_in _out _err _ -> return ()
+            -- WIP
+            $ \_in _out _err ph -> waitForProcess ph
+            
+    whenJust (Just a) f = f a
+    whenJust Nothing  _ = return ()
 
-    -- WIP
-    runWindowUpdater path args archive =
-        withCreateProcess (proc path (args <> archive))
-            $ \_in _out _err _ -> return ()
+writeWindowsUpdaterRunner :: FilePath -> IO ()
+writeWindowsUpdaterRunner runnerPath = do
+    exePath <- getExecutablePath
+    launcherArgs <- getArgs
+#ifdef mingw32_HOST_OS
+    selfPid <- getCurrentProcessId
+#else
+    let (selfPid :: Integer) = 0 -- This will never be run on non-Windows
+#endif
+    writeFile (toS runnerPath) $ T.unlines
+        [ "TaskKill /PID "<> show selfPid <>" /F"
+        -- Run updater
+        , "%*"
+        -- Delete updater
+        , "del %1"
+        -- Run launcher again
+        , "start \"cardano launcher\" /b " <> (quote $ toS exePath) <> " "
+            <> (T.unwords $ map (quote . toS) launcherArgs)
+        -- Delete the bat file
+        , "(goto) 2>nul & del \"%~f0\""
+        ]
+  where
+    quote :: Text -> Text
+    quote str = "\"" <> str <> "\""
