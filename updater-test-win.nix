c45f70a3a987286c3e140abab120531d52c4ba0f
Hiroto Shioi <hiroto.shioi@iohk.io>
2019-09-02 12:27:56 +0900

    Add libffi-6 on updater directory

--- updater-test-win.nix
+++ updater-test-win.nix
@@ -9,18 +9,19 @@ let
   crossSelf = import ./. { crossSystem = pkgs.lib.systems.examples.mingwW64; };
   pkgsCross = commonLib.getPkgs { crossSystem = pkgs.lib.systems.examples.mingwW64; };
 in pkgs.runCommand "updater_test.zip" { buildInputs = [ pkgs.zip ]; } ''
-  mkdir -pv $out/product
-  cd $out/product
+  mkdir -pv $out/updater-test
+  cd $out/updater-test
   mkdir -pv configuration/client configuration/server configuration/launcher updater
   cp ${crossSelf.nix-tools.cexes.cardano-launcher.cardano-launcher}/bin/cardano-launcher.exe ./cardano-launcher.exe
   cp ${crossSelf.nix-tools.cexes.cardano-launcher.mock-daedalus-frontend}/bin/mock-daedalus-frontend.exe ./daedalus.exe
   cp ${crossSelf.nix-tools.cexes.cardano-launcher.mock-installer}/bin/mock-installer.exe ./updater/updater.exe
   cp ${pkgsCross.libffi}/bin/libffi-6.dll ./
+  cp ${pkgsCross.libffi}/bin/libffi-6.dll ./updater/
   cp ${./configuration/launcher/launcher-config.demo.yaml} ./configuration/launcher/launcher-config.demo.yaml
   cp ${./configuration/cert-configuration.yaml} ./configuration/cert-configuration.yaml
   cp ${./configuration/log-configuration.yaml} ./configuration/log-configuration.yaml
   cd ..
-  zip updater_test.zip -r product/
+  zip updater_test.zip -r updater-test/
 ''
 
 # This script fails because we do not have https://github.com/input-output-hk/cardano-shell/pull/255 merged yet!
