dee3f3abe20313082cea9748a1db8810e359d0cf
Hiroto Shioi <hiroto.shioi@iohk.io>
2019-09-17 10:09:06 +0900

    Implement test case

--- cardano-launcher/test/Spec.hs
+++ cardano-launcher/test/Spec.hs
@@ -3,6 +3,7 @@ module Main where
 import           Cardano.Prelude
 import           LauncherSMSpec (launcherSMSpec)
 import           LauncherSpec (launcherSpec)
+import           TemplateSpec (templateSpec)
 import           Test.Hspec (describe, hspec)
 import           UpdaterSpec (updaterSpec)
 
@@ -12,4 +13,5 @@ main = hspec $ do
     describe "Update spec" updaterSpec
     describe "Launcher spec" launcherSpec
     describe "LauncherSM spec" launcherSMSpec
+    describe "Template spec" templateSpec
 
