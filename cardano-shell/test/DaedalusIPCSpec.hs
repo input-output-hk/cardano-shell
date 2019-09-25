d9f23b58b6b2f8a5f39b44f1b98280e8707f9736
ksaric <kristijan.saric@iohk.io>
2019-09-25 09:30:47 +0200

    [GH-287] Try to fix failing test, corrected path.

--- cardano-shell/test/DaedalusIPCSpec.hs
+++ cardano-shell/test/DaedalusIPCSpec.hs
@@ -12,6 +12,6 @@ spec :: Spec
 spec = describe "DaedalusIPC" $ do
     it "should reply with the port when asked" $ do
         let port = 42 :: Int
-        let testScript = "test/js/mock-daedalus.js"
+        let testScript = "cardano-shell/test/js/mock-daedalus.js"
         (_, _, _, ph) <- createProcess (proc "node" [testScript, show port])
         waitForProcess ph `shouldReturn` ExitSuccess
