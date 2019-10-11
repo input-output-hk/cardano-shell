356e754c4a8b635debb019cb89b3e56ec67fc12c
Hiroto Shioi <hiroto.shioi@iohk.io>
2019-10-11 17:02:21 +0900

    Exception handling upon certificate generation (#309)
    * Exception handling upon certificate generation
    
    * Implement test case

--- cardano-launcher/app/Main.hs
+++ cardano-launcher/app/Main.hs
@@ -6,10 +6,8 @@ module Main where
 import           Cardano.Prelude
 import qualified Prelude
 
-import           System.Directory (createDirectoryIfMissing)
 import           System.Environment (setEnv)
 import           System.Exit (exitWith)
-import           System.FilePath ((</>))
 
 import           Formatting (bprint, build, formatToString)
 import           Formatting.Buildable (Buildable (..))
@@ -25,20 +23,15 @@ import           Cardano.Shell.Configuration (ConfigurationOptions (..),
                                               WalletPath (..), getUpdaterData,
                                               getWPath, getWargs,
                                               setWorkingDirectory)
-import           Cardano.Shell.Launcher (LoggingDependencies (..),
-                                         WalletMode (..), runWalletProcess,
-                                         walletRunnerProcess)
+import           Cardano.Shell.Launcher (LoggingDependencies (..), TLSError,
+                                         TLSPath (..), WalletMode (..),
+                                         generateTlsCertificates,
+                                         runWalletProcess, walletRunnerProcess)
 import           Cardano.Shell.Update.Lib (UpdaterData (..),
                                            runDefaultUpdateProcess, runUpdater)
-import           Cardano.X509.Configuration (ConfigurationKey (..),
-                                             DirConfiguration (..), certChecks,
-                                             certFilename, certOutDir,
-                                             decodeConfigFile,
-                                             fromConfiguration, genCertificate)
 import           Control.Exception.Safe (throwM)
-import           Data.X509.Extra (failIfReasons, genRSA256KeyPair,
-                                  validateCertificate, writeCertificate,
-                                  writeCredentials)
+import           Data.Text.Lazy.Builder (fromString, fromText)
+
 --------------------------------------------------------------------------------
 -- Main
 --------------------------------------------------------------------------------
@@ -94,10 +87,14 @@ main = do
     -- | If we need to, we first check if there are certificates so we don't have
     -- to generate them. Since the function is called `generate...`, that's what
     -- it does, it generates the certificates.
-    generateTlsCertificates
+    eTLSGeneration <- generateTlsCertificates
         loggingDependencies
         configurationOptions
         tlsPath
+
+    case eTLSGeneration of
+        Left generationError -> throwM $ FailedToGenerateTLS generationError
+        Right _              -> return ()
 
     -- In the case the user wants to avoid installing the update now, we
     -- run the update (if there is one) when we have it downloaded.
@@ -119,16 +116,6 @@ main = do
     exitWith exitCode
 
 --------------------------------------------------------------------------------
--- Types
---------------------------------------------------------------------------------
-
-newtype X509ToolPath = X509ToolPath { getX509ToolPath :: FilePath }
-    deriving (Eq, Show)
-
-newtype TLSPath = TLSPath { getTLSFilePath :: FilePath }
-    deriving (Eq, Show)
-
---------------------------------------------------------------------------------
 -- Exceptions
 --------------------------------------------------------------------------------
 
@@ -136,7 +123,7 @@ data LauncherExceptions
     = CannotGenerateTLS
     | LauncherOptionsError Text
     | WorkingDirectoryDoesNotExist FilePath
-    deriving (Eq)
+    | FailedToGenerateTLS TLSError
 
 instance Buildable LauncherExceptions where
     build = \case
@@ -145,87 +132,15 @@ instance Buildable LauncherExceptions where
             \ and make sure you aren't already running an instance of Daedalus wallet."
         LauncherOptionsError err -> bprint
                "Error occured during loading configuration file:\n"
-            <> Formatting.Buildable.build err
+            <> fromText err
         WorkingDirectoryDoesNotExist path -> bprint
                "Failed to set working directory because it does not exist: "
-            <> Formatting.Buildable.build path
+            <> fromString path
+        FailedToGenerateTLS tlsError -> bprint
+               "Failed to generate tls certificate due to error:\n"
+            <> fromString (show tlsError)
 
 instance Show LauncherExceptions where
     show = formatToString Formatting.build
 
 instance Exception LauncherExceptions
-
---------------------------------------------------------------------------------
--- Functions
---------------------------------------------------------------------------------
-
--- | Utility function.
-textToFilePath :: Text -> FilePath
-textToFilePath = strConv Strict
-
--- | Generation of the TLS certificates.
--- This just covers the generation of the TLS certificates and nothing else.
-generateTlsCertificates :: LoggingDependencies -> ConfigurationOptions -> TLSPath -> IO ()
-generateTlsCertificates externalDependencies' configurationOptions (TLSPath tlsPath) = do
-
-    let tlsServer = tlsPath </> "server"
-    let tlsClient = tlsPath </> "client"
-
-    logInfo externalDependencies' $ "Generating the certificates!"
-
-    -- Create the directories.
-    createDirectoryIfMissing True tlsServer
-    createDirectoryIfMissing True tlsClient
-
-    -- Generate the certificates.
-    generateCertificates tlsServer tlsClient
-        `onException` throwM CannotGenerateTLS
-        -- ^ unfortunate since we gobble up the real cause,
-        -- but we want the client to have a nice message
-
-  where
-    -- | The generation of the certificates. More or less copied from
-    -- `cardano-sl`.
-    generateCertificates :: FilePath -> FilePath -> IO ()
-    generateCertificates tlsServer' tlsClient = do
-
-        let configFile = cfoFilePath configurationOptions
-        -- | Configuration key within the config file
-        let configKey :: ConfigurationKey
-            configKey = ConfigurationKey . textToFilePath . cfoKey $ configurationOptions
-
-        let outDirectories :: DirConfiguration -- ^ Output directories configuration
-            outDirectories = DirConfiguration
-                { outDirServer  = tlsServer'
-                , outDirClients = tlsClient
-                , outDirCA      = Nothing -- TODO(KS): AFAIK, we don't output the CA.
-                }
-
-        -- | TLS configuration
-        tlsConfig <- decodeConfigFile configKey configFile
-
-        -- | From configuraiton
-        (caDesc, descs) <-
-            fromConfiguration tlsConfig outDirectories genRSA256KeyPair <$> genRSA256KeyPair
-
-        let caName      = certFilename caDesc
-
-        let serverHost  = "localhost"
-        let serverPort  = ""
-
-        -- | Generation of the certificate
-        (caKey, caCert) <- genCertificate caDesc
-
-        case certOutDir caDesc of
-            Nothing  -> return ()
-            Just dir -> writeCredentials (dir </> caName) (caKey, caCert)
-
-        forM_ descs $ \desc -> do
-            (key, cert) <- genCertificate desc
-            failIfReasons =<< validateCertificate
-                caCert
-                (certChecks desc)
-                (serverHost, serverPort)
-                cert
-            writeCredentials (certOutDir desc </> certFilename desc) (key, cert)
-            writeCertificate (certOutDir desc </> caName) caCert
